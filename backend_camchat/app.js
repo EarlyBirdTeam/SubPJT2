2c1cf61dc1c73b9d43ff8e6e66966d2464e7506e
ruylruyl <44992114+dongryul123@users.noreply.github.com>
2020-07-31 00:37:06 +0900

    index.html in camchat

--- backend_camchat/app.js
+++ backend_camchat/app.js
@@ -8,7 +8,7 @@ var socketIO = require('socket.io');
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8000);
+}).listen(8001);
 
 var io = socketIO.listen(app);
 
